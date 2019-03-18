67d96a458d505b6d408c4f6729ce4f1768107db6
Jeff Albrecht <jeff.albrecht@slingshotaerospace.com>
2019-03-18 12:28:53 -0600

    updating tests and reqs

--- tests.py
+++ tests.py
@@ -1,8 +1,37 @@
-# from datasources.tests import BaseTestCases
-import unittest
+from datasources import tests
+
+from Landsat8 import Landsat8
 
 
-class Landsat8TestCases(unittest.TestCase):
+class Landsat8TestCases(tests.BaseTestCases):
 
-    def test_fake(self):
-        self.assertEqual(1,1)
+    def _setUp(self):
+        self.datasource = Landsat8
+        self.spatial = {
+                        "type": "Polygon",
+                        "coordinates": [
+                          [
+                            [
+                              -96.84173583984374,
+                              32.713355353177555
+                            ],
+                            [
+                              -96.63848876953125,
+                              32.713355353177555
+                            ],
+                            [
+                              -96.63848876953125,
+                              32.88189375925038
+                            ],
+                            [
+                              -96.84173583984374,
+                              32.88189375925038
+                            ],
+                            [
+                              -96.84173583984374,
+                              32.713355353177555
+                            ]
+                          ]
+                        ]
+                      }
+        self.temporal = ("2017-01-01", "2017-12-31")
