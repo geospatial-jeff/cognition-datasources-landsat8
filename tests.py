60c18fb49e95f042f3dd94597ff13f5c7f172662
Jeff Albrecht <jeff.albrecht@slingshotaerospace.com>
2019-03-22 23:40:27 -0600

    bringing back test cases

--- tests.py
+++ tests.py
@@ -1,48 +1,39 @@
 from datasources import tests
 
-import datasources
-
-from datasources import sources
+from Landsat8 import Landsat8
 
 
-print(dir(datasources))
-print(dir(sources))
+class Landsat8TestCases(tests.BaseTestCases):
 
-
-# from Landsat8 import Landsat8
-#
-#
-# class Landsat8TestCases(tests.BaseTestCases):
-#
-#     def _setUp(self):
-#         self.datasource = Landsat8
-#         self.spatial = {
-#                         "type": "Polygon",
-#                         "coordinates": [
-#                           [
-#                             [
-#                               -96.84173583984374,
-#                               32.713355353177555
-#                             ],
-#                             [
-#                               -96.63848876953125,
-#                               32.713355353177555
-#                             ],
-#                             [
-#                               -96.63848876953125,
-#                               32.88189375925038
-#                             ],
-#                             [
-#                               -96.84173583984374,
-#                               32.88189375925038
-#                             ],
-#                             [
-#                               -96.84173583984374,
-#                               32.713355353177555
-#                             ]
-#                           ]
-#                         ]
-#                       }
-#         self.temporal = ("2017-01-01", "2017-12-31")
-#         self.properties = {"landsat:processing_level": {"eq": "L1TP"}}
-#         self.limit = 20
+    def _setUp(self):
+        self.datasource = Landsat8
+        self.spatial = {
+                        "type": "Polygon",
+                        "coordinates": [
+                          [
+                            [
+                              -96.84173583984374,
+                              32.713355353177555
+                            ],
+                            [
+                              -96.63848876953125,
+                              32.713355353177555
+                            ],
+                            [
+                              -96.63848876953125,
+                              32.88189375925038
+                            ],
+                            [
+                              -96.84173583984374,
+                              32.88189375925038
+                            ],
+                            [
+                              -96.84173583984374,
+                              32.713355353177555
+                            ]
+                          ]
+                        ]
+                      }
+        self.temporal = ("2017-01-01", "2017-12-31")
+        self.properties = {"landsat:processing_level": {"eq": "L1TP"}}
+        self.limit = 20
