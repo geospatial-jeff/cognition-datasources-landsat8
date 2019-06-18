765c8af9c5952000455dec013c604a9a4c556f58
Jeff Albrecht <geospatialjeff@gmail.com>
2019-06-17 20:43:47 -0700

    updating to earth search

--- Landsat8.py
+++ Landsat8.py
@@ -11,18 +11,19 @@ class Landsat8(Datasource):
 
     def __init__(self, manifest):
         super().__init__(manifest)
-        self.endpoint = 'https://sat-api-dev.developmentseed.org/stac/search/'
+        self.endpoint = 'https://earth-search.aws.element84.com/stac/search'
 
     def search(self, spatial, temporal=None, properties=None, limit=10, **kwargs):
         stac_query = STACQuery(spatial, temporal)
 
-        query_body = {'query': {'eo:platform': {'eq': 'landsat-8'}},
-                      'limit': limit,
-                      'intersects': json.dumps({
-                          "type": "Feature",
-                          "properties": {},
-                          "geometry": stac_query.spatial
-                      })}
+        query_body = {
+            'query': {
+                'collection': {
+                    'eq': 'landsat-8-l1'
+                }
+            },
+            'intersects': stac_query.spatial
+        }
 
         if temporal:
             query_body.update({'time': "/".join([x.strftime("%Y-%m-%dT%H:%M:%S.%fZ") for x in stac_query.temporal])})
@@ -31,7 +32,6 @@ class Landsat8(Datasource):
             for (k,v) in properties.items():
                 query_body['query'].update({k:v})
 
-        query_body['query'] = json.dumps(query_body['query'])
         self.manifest.searches.append([self, query_body])
 
     def execute(self, query):
@@ -39,7 +39,7 @@ class Landsat8(Datasource):
             "ContentType": "application/json",
             "Accept": "application/geo+json"
         }
-        r = requests.get(self.endpoint, params=query, headers=headers)
+        r = requests.post(self.endpoint, data=json.dumps(query), headers=headers)
         return r.json()
 
     def example(self):
